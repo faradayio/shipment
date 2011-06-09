a1efeab69dd31b1a36b451aa250ba8e791614bf6
Seamus Abshere <seamus@abshere.net>
2011-06-09 01:24:17 -0500

    make tests green with new sniff

--- lib/shipment/data.rb
+++ lib/shipment/data.rb
@@ -2,22 +2,18 @@ module BrighterPlanet
   module Shipment
     module Data
       def self.included(base)
-        base.data_miner do
-          schema do
-            float   'weight'
-            integer 'package_count'
-            string  'carrier_name'
-            string  'mode_name'
-            integer 'segment_count'
-            string  'origin'
-            string  'destination'
-            string  'origin_zip_code'      # For backwards compatability
-            string  'destination_zip_code' # For backwards compatability
-            float   'distance'
-            string  'mapquest_api_key'
-          end
-          
-          process :run_data_miner_on_belongs_to_associations
+        base.create_table do
+          float   'weight'
+          integer 'package_count'
+          string  'carrier_name'
+          string  'mode_name'
+          integer 'segment_count'
+          string  'origin'
+          string  'destination'
+          string  'origin_zip_code'      # For backwards compatability
+          string  'destination_zip_code' # For backwards compatability
+          float   'distance'
+          string  'mapquest_api_key'
         end
       end
     end
