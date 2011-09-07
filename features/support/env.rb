4c2f2bade0ff7846d3a256edc405dd98e06ec3e8
Ian <ian@worklap.local>
2011-09-07 19:47:12 +0200

    Log test output

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,6 +11,6 @@ require 'data_miner'
 DataMiner.logger = Logger.new nil
 
 require 'sniff'
-Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :cucumber => true, :earth => [:shipping]
+Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :cucumber => true, :earth => [:shipping], :logger => 'log/test_log.txt'
 
 MAPQUEST_KEY = 'ABC123'
