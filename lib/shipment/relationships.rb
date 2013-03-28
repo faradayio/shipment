fc31d93ec560446162a09036c53a51ad05d8e9b9
Seamus Abshere <seamus@abshere.net>
2013-03-28 15:22:07 -0500

    forgot this

--- lib/shipment/relationships.rb
+++ lib/shipment/relationships.rb
@@ -1,3 +1,4 @@
+require 'earth/locality/country'
 require 'earth/shipping/carrier'
 require 'earth/shipping/shipment_mode'
 
@@ -5,6 +6,7 @@ module BrighterPlanet
   module Shipment
     module Relationships
       def self.included(base)
+        base.belongs_to :country, :foreign_key => 'country_iso_3166_code'
         base.belongs_to :carrier, :foreign_key => 'carrier_name',  :primary_key => 'name'
         base.belongs_to :mode,    :foreign_key => 'mode_name',     :primary_key => 'name', :class_name => 'ShipmentMode'
       end
